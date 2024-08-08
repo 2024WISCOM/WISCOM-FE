d14c1758031e4fb79d4f8a61c7afcdb020ddfe0e
7beunseo <kimes0403@gmail.com>
2024-08-08 19:25:13 +0900

    Feat: PC 버전 Header UI 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 import Header from './components/header/Header';
 import Main from './pages/Main';
 import About from './pages/About';
@@ -7,16 +8,24 @@ import WorkDetail from './pages/WorkDetail';
 import GuestBook from './pages/GuestBook';
 
 function App() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/work-list" element={<WorkList />} />
-        <Route path="/work-detail" element={<WorkDetail />} />
-        <Route path="/guestbook" element={<GuestBook />} />
-      </Routes>
+      <Header isVisible={isVisible} toggleMenu={toggleMenu} />
+      {!isVisible && (
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/work-list" element={<WorkList />} />
+          <Route path="/work-detail" element={<WorkDetail />} />
+          <Route path="/guestbook" element={<GuestBook />} />
+        </Routes>
+      )}
     </BrowserRouter>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,64 @@
+import React, { useCallback, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import * as H from './Header.style';
 
-export default function Header() {
+export default function Header({ isVisible, toggleMenu }) {
+  const [isAnimating, setIsAnimating] = useState(isVisible);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isVisible) {
+      setIsAnimating(true);
+    } else {
+      const timer = setTimeout(() => setIsAnimating(false), 500);
+      return () => clearTimeout(timer);
+    }
+  }, [isVisible]);
+
+  const menuItems = [
+    { name: 'HOME', path: '/' },
+    { name: 'ABOUT', path: '/about' },
+    { name: 'WORKS', path: '/work-list' },
+    { name: 'GUESTBOOK', path: '/guestbook' },
+  ];
+
+  const handleMouseMove = useCallback((event, item) => {
+    const rect = item.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    const percentageX = (x / rect.width) * 100;
+    const percentageY = (y / rect.height) * 100;
+
+    item.style.setProperty('--mouse-x', `${percentageX}%`);
+    item.style.setProperty('--mouse-y', `${percentageY}%`);
+  }, []);
+
+  const handleNavigation = (path) => {
+    navigate(path);
+    toggleMenu();
+  };
+
   return (
-    <H.Container>
-      <H.Logo>WISCOM</H.Logo>
-    </H.Container>
+    <H.HeaderContainer>
+      <H.Logo onClick={() => navigate('/')}>WISCOM</H.Logo>{' '}
+      {/* Logo navigation */}
+      <H.MenuContainer onClick={toggleMenu}>
+        <H.MENU>{isVisible ? 'CLOSE' : 'MENU'}</H.MENU>
+        <H.HamburgerIcon isVisible={isVisible} />
+      </H.MenuContainer>
+      <H.NavMenu isVisible={isAnimating}>
+        {menuItems.map((item, index) => (
+          <H.NavItem
+            key={item.name}
+            isVisible={isVisible}
+            index={index}
+            onMouseMove={(e) => handleMouseMove(e, e.currentTarget)}
+            onClick={() => handleNavigation(item.path)}
+          >
+            {item.name}
+          </H.NavItem>
+        ))}
+      </H.NavMenu>
+    </H.HeaderContainer>
   );
 }
