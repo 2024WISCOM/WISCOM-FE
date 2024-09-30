e4050ad94b76cc9c7603a58ed0823692f74447be
techncherry <technarchitect@gmail.com>
2024-10-01 00:17:50 +0900

    ✨ Feat: 카카오맵 연결

---
+++ src/components/about/Kakaomap.jsx
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+
+const { kakao } = window;
+
+export default function Kakaomap() {
+  useEffect(() => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new kakao.maps.LatLng(37.655336291520875, 127.04801288120866),
+      level: 3,
+    };
+
+    const map = new kakao.maps.Map(container, options);
+
+    const markerPosition = new kakao.maps.LatLng(
+      37.655336291520875,
+      127.04801288120866,
+    );
+
+    const marker = new kakao.maps.Marker({
+      position: markerPosition,
+    });
+    marker.setMap(map);
+  }, []);
+
+  return (
+    <div
+      id="map"
+      style={{
+        width: '40%',
+        height: '550px',
+        margin: '0',
+      }}
+    ></div>
+  );
+}
