e1c95e4590fea6476d46e1fc7f4cea7bb9b33032
nadomolayohehe <siyun778@naver.com>
2024-10-02 18:34:40 +0900

    :lipstick: 모바일 ui 수정
    모바일 ui 수정

--- src/components/guestBook/AddMessage.jsx
+++
@@ -1,9 +0,0 @@
-import { useNavigate } from "react-router-dom"
-import { useContext } from "react"
-
-
-export const AddMessage = () =>{
-    const {onCreate} = useContext(DiaryD)
-
-}
-
--- src/pages/GuestBook.jsx
+++ src/pages/GuestBook.jsx
@@ -1,4 +1,4 @@
-import { AddMessage } from "../components/guestBook/addMessage";
+
 
 export default function GuestBook() {
   return (
