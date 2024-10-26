54ef62bcc60facf62587c16d8609b4232a41397b
7beunseo <kimes0403@gmail.com>
2024-10-26 21:39:25 +0900

    🐛 Fix: 데이터 변경되면 슬라이드 위로 올림

--- src/components/workdetail/leftcomponent/LeftContent.jsx
+++ src/components/workdetail/leftcomponent/LeftContent.jsx
@@ -1,9 +1,18 @@
+import { useEffect, useRef } from 'react';
 import * as L from './LeftContent.style';
 import Image from '../image/Image';
+
 export default function LeftContent({ images }) {
-  console.log(images);
+  const containerRef = useRef(null); 
+
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollTop = 0; 
+    }
+  }, [images]); 
+
   return (
-    <L.LeftContentContainer>
+    <L.LeftContentContainer ref={containerRef}>
       {images.map((item, index) => (
         <Image key={index} src={item.url} />
       ))}
